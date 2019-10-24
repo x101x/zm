0b3263663c317cf8ca29d7cad9d86fa644a64b69
x101x <1642372486@qq.com>
2019-10-24 16:10:04 +0800

    EventEmitter

---
+++ EventEmitter/base.js
@@ -0,0 +1,24 @@
+const Event = require('events');
+const ev = new Event();
+// 订阅
+function price1() {
+    console.log('大米涨价了！');
+}
+ev.on('price', price1)
+ev.on('price', function(thing) {
+    console.log(`${thing}涨价了`);
+})
+// 发布者
+ev.emit('price', '大蒜');
+ev.removeListener('price', price1);  // 取消订阅
+ev.emit('price', '大蒜');
+
+//once  只订阅一次(只接收一次发布者的消息)
+ev.once('eat', () => {
+    console.log('eat1');
+})
+ev.once('eat', () => {
+    console.log('eat2');
+})
+ev.emit('eat');
+ev.emit('eat');
